d868f971744fc0b3bdcd4ef2e9c074038bf3d4e9
hadi dasti <hadidasti98@hotmail.com>
2023-01-18 13:29:59 +0330

    create new file in admin project

--- admin.js
+++ admin.js
@@ -1,14 +1,18 @@
-require('dotenv').config({})
+
 const express = require('express')
 
+
+require('dotenv').config()
+
 const app = express()
+const {PORT} = require('./config')
 
 
 //middleware
 app.use(express.json())
 
+app.use('/admin',require('./router'))
 
 
-
-app.listen(3000,()=> console.log('start  dashboard panel Admin'))
+app.listen(PORT,()=> console.log(`start  dashboard panel Admin with port ${PORT}`))
 
